e809318bf90bd59e7174322f658fe30197d5f135
Ievgen Kyvgyla <Ievgen_Kyvgyla@epam.com>
2020-11-13 09:25:07 +0200

    Updates resolvers

--- data/resolvers.js
+++ data/resolvers.js
@@ -1,12 +1,20 @@
 import mongoose from 'mongoose';
-import { Friends } from './dbConnectors';
+import { Friends, Aliens } from './dbConnectors';
 
 // resolver map
 export const resolvers = {
     Query: {
-        getFriend: ({ id }) => {
-            return new Friend(id, friendDatabase[id]);
+        getOneFriend: (root, { id }) => {
+            return new Promise((resolve, object) => {
+                Friends.findById(id, (err, friend) => {
+                    if (err) reject(err)
+                    else resolve(friend)
+                });
+            });
         },
+        getAliens: () => {
+            return Aliens.findAll();
+        }
     },
     Mutation: {
         createFriend: (root, { input }) => {
@@ -27,7 +35,23 @@ export const resolvers = {
                     if (err) reject(err)
                     else resolve(newFriend)
                 })
+            }) 
+        },
+        updateFriend: (root, { input }) => {
+            return new Promise(( resolve, object) => {
+                Friends.findOneAndUpdate({ _id: input.id }, input, { new: true}, (err, friend) => {
+                    if (err) reject(err)
+                    else resolve(friend)
+                })
             })
         },
+        deleteFriend: (root, { id }) => {
+            return new Promise((resolve, object) => {
+                Friends.remove({ _id: id}, (err) => {
+                    if (err) reject(err)
+                    else resolve('Successfully deleted friend')
+                })
+            })
+        }
     },
 };
